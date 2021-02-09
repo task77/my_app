8e01aa7870a53ce44bd824f7c958bded7f41161b
task77 <taskshima@gmail.com>
2021-02-10 06:40:44 +0900

    社員登録画の修正

--- resources/views/employee_create.blade.php
+++ resources/views/employee_create.blade.php
@@ -4,37 +4,100 @@
 <div class="container">
     <div class="login-container">
     <h3>新規社員登録</h3>
-                <div class="card-body">
-                    <form method="POST" action="{{ route('employee_create.store') }}"> 
-                        @csrf
+        <div class="card-body">
+            <form method="POST" action="{{ route('employee_create.store') }}"> 
+                @csrf
 
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label text-md-right">社員番号</label>
-                                <div class="col-md-6">
-                                    <input type="text" name="employee_id" class="form-control" value="{{old('employee_id')}}" placeholder="社員番号を入力してください">
-                                </div>
+                <div class="form-group row">
+                    <label class="col-md-4 col-form-label text-md-right">社員番号</label>
+                        <div class="col-md-6">
+                            <input type="text" name="employee_id" class="form-control" value="{{old('employee_id')}}" placeholder="社員番号を入力してください">
                         </div>
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label text-md-right">名前</label>
-                                <div class="col-md-6">
-                                    <input type="tel" name="employee_name" class="form-control" value="{{old('employee_name')}}" placeholder="名前を入力してください">
-                                </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label text-md-right">所属</label>
-                                <div class="col-md-6">
-                                    <input type="text" name="office" class="form-control" value="{{old('office')}}" placeholder="所属部署を入力してください">
-                                    <!-- プルダウンでの選択式だと使いやすいと思う -->
-                                </div>
-                        </div>
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <input type="submit" value="社員登録する" class="btn btn-primary">
-                                <input type="reset" value="キャンセル" class="btn btn-secondary" onclick='window.history.back(-1);'>
-                            </div>
-                        </div>
-                    </form>
                 </div>
+                <div class="form-group row">
+                    <label class="col-md-4 col-form-label text-md-right">名前</label>
+                        <div class="col-md-6">
+                            <input type="tel" name="employee_name" class="form-control" value="{{old('employee_name')}}" placeholder="名前を入力してください">
+                        </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-4 col-form-label text-md-right">所属事業所</label>
+                        <div class="col-md-6">
+                            <select name="office" class="form-control">
+                            <option value="">所属事業所を選択してください</option>
+                            <option value="新潟営業所">新潟営業所</option>
+                            <option value="長岡営業所">長岡営業所</option>
+                            <option value="長岡営業所">上越営業所</option>
+                            <option value="長岡営業所">群馬営業所</option>
+                            <option value="長岡営業所">長野営業所</option>
+                            </select>
+                        </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-4 col-form-label text-md-right">制服</label>
+                        <div class="col-md-6">
+                            <select name="uniform" class="form-control">
+                            <option value="">制服のサイズを選択してください</option>
+                            <option value="S">S</option>
+                            <option value="M">M</option>
+                            <option value="L">L</option>
+                            <option value="XL">XL</option>
+                            </select>
+                        </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-4 col-form-label text-md-right">防寒着</label>
+                        <div class="col-md-6">
+                            <select name="winter_clothes" class="form-control">
+                            <option value="">防寒着のサイズを選択してください</option>
+                            <option value="S">S</option>
+                            <option value="M">M</option>
+                            <option value="L">L</option>
+                            <option value="XL">XL</option>
+                            </select>
+                        </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-4 col-form-label text-md-right">靴</label>
+                        <div class="col-md-6">
+                            <select name="shoes" class="form-control">
+                            <option value="">靴のサイズを選択してください</option>
+                            <option value="23㎝">23㎝</option>
+                            <option value="23.5㎝">23.5㎝</option>
+                            <option value="24㎝">24㎝</option>
+                            <option value="24.5㎝">24.5㎝</option>
+                            <option value="25㎝">25㎝</option>
+                            <option value="25.5㎝">25.5㎝</option>
+                            <option value="26㎝">26㎝</option>
+                            <option value="26.5㎝">26.5㎝</option>
+                            <option value="27㎝">27㎝</option>
+                            <option value="27.5㎝">27.5㎝</option>
+                            <option value="28㎝">28㎝</option>
+                            <option value="28.5㎝">28.5㎝</option>
+                            </select>
+                        </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-4 col-form-label text-md-right">その他</label>
+                        <div class="col-md-6">
+                            <input type="text" name="other" class="form-control" value="{{old('other')}}" placeholder="その他を入力してください">
+                        </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-4 col-form-label text-md-right">メモ</label>
+                        <div class="col-md-6">
+                            <input type="text" name="memo" class="form-control" value="{{old('memo')}}" placeholder="メモを入力してください">
+                        </div>
+                </div>
+
+                <div class="form-group row mb-0">
+                    <div class="col-md-8 offset-md-4">
+                        <input type="submit" value="社員登録する" class="btn btn-primary">
+                        <input type="reset" value="戻る" class="btn btn-secondary" onclick='window.history.back(-1);'>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,34 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-
   <table class="table table-hover">
     <thead class="thead-dark">
       <tr>
         <th scope="col">社員番号</th>
-        <th scope="col">氏名</th>
+        <th scope="col">名前</th>
+        <th scope="col">所属事業所</th>
         <th scope="col" colspan="4">貸与品</th>
+        <th scope="col"></th>
       </tr>
     </thead>
-    <tbody>
-      <tr>
-        <td>020990</td>
-        <td>佐藤翼</td>
-        <td>制服</td>
-        <td>防寒着</td>
-        <td>靴</td>
-        <td>その他</td>
-      </tr>
-    </tbody>
 
     <tbody>
     @foreach($employees as $employee)
       <tr>
         <td scope="row">{{ $employee->employee_id }}</td>
         <td scope="row">{{ $employee->employee_name }}</td>
+        <td scope="row">{{ $employee->office }}</td>
+        <td>
+          @foreach($employee->goods as $goods)
+            <p>制服：{{ $goods->uniform }}｜防寒着：{{ $goods->winter_clothes }}｜靴：{{ $goods->shoes }}｜その他：{{ $goods->other }}</p>
+          @endforeach
+        </td>
+        <td scope="row"><a href="{{ route('employee_create.show',$employee->id) }}" class="btn btn-primary">詳細</a></td>
       </tr>
     @endforeach
     </tbody>
   </table>
-
 @endsection
