e4a6bd8709c671c87a7e6655e41685856bcb0a9c
task77 <taskshima@gmail.com>
2021-02-15 17:33:28 +0900

    ルートの修正

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', 'EmployeesController@index');
 
 Auth::routes();
@@ -20,6 +9,4 @@ Route::get('index', 'HomeController@index')->name('index');
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('index', 'EmployeesController', ['only' => ['index']]);
     Route::resource('employee_create', 'EmployeesController');
-    //Route::resource('employee_create', 'EmployeesController', ['only' => ['index', 'create','store','show','edit','update','destroy']]);
-    //Route::resource('goods', 'GoodsController', ['only' => ['create', 'store', 'destroy']]);
 });
