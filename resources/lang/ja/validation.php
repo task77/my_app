8c84a815d0fa55d2d0d042da41dd4d1d12397b7e
task77 <taskshima@gmail.com>
2021-02-15 17:36:12 +0900

    バリデーションの修正

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -145,6 +145,14 @@ return [
     |
     */
 
-    'attributes' => [],
+    'attributes' => [
+        'employee_id' => '社員番号',
+        'employee_name' => '社員の名前',
+        'office' => '所属事業所',
+        'uniform' => '制服',
+        'winter_clothes' => '防寒着',
+        'shoes' => '靴',
+        'other' => 'その他',
+    ],
 
 ];
