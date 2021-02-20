eaa96c2712c6b7688a8f83ea79b7b037c57c1cbf
task77 <taskshima@gmail.com>
2021-02-21 07:49:55 +0900

    リファクタリング

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -1,52 +1,53 @@
 <header class="py-4">
 
     <nav class="navbar navbar-expand-md navbar-dark mt-3 mb-3" style="background-color:#58BA9D">
-            <a class="navbar-brand" href="{{ url('/') }}">
-            <i class="fab fa-pagelines"></i> MANAGE
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <a class="navbar-brand" href="{{ url('/') }}">
+        <i class="fab fa-pagelines"></i> MANAGE
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto"></ul>
-                <ul class="nav navbar-nav navbar-right">
-                    @guest
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="nav navbar-nav navbar-right">
+                @guest
+                    <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}"><i class="fas fa-sign-in-alt"></i> {{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <button class="btn btn-success">
+                        <a href="{{ route('login.guest') }}" class="text-white"><i class="fas fa-sign-in-alt"></i> ゲストログイン</a>
+                        </button>
                         <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}"><i class="fas fa-sign-in-alt"></i> {{ __('Login') }}</a>
+                            <a class="nav-link" href="{{ route('register') }}"><i class="fas fa-smile"></i> {{ __('Register') }}</a>
                         </li>
-                        @if (Route::has('register'))
-                            <button class="btn btn-success">
-                            <a href="{{ route('login.guest') }}" class="text-white"><i class="fas fa-sign-in-alt"></i> ゲストログイン</a>
-                            </button>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}"><i class="fas fa-smile"></i> {{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
+                    @endif
+                @else
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('employee_create.create') }}"><i class="fas fa-user-plus"></i> {{ __('新規社員登録') }}</a>
-                        </li>
-                        
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            <i class="fas fa-user-check"></i> {{ Auth::user()->name }} <span class="caret"></span>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('employee_create.create') }}"><i class="fas fa-user-plus"></i> {{ __('新規社員登録') }}</a>
+                    </li>
+                    
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <i class="fas fa-user-check"></i> {{ Auth::user()->name }} <span class="caret"></span>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                    <i class="fas fa-sign-out-alt"></i> {{ __('ログアウト') }}
-                                </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                <i class="fas fa-sign-out-alt"></i> {{ __('ログアウト') }}
+                            </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
     </nav>
 
 </header>
