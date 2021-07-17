271d4a9e867e19a994f1396e2cc7093fa13a826c
task77 <taskshima@gmail.com>
2021-07-17 10:15:58 +0900

    画像項目の追加

--- app/Employee.php
+++ app/Employee.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
 
-    protected $fillable = ['id','emlpoyee_id','employee_name','office'];
+    protected $fillable = ['id','emlpoyee_id','employee_name','office','employee_image'];
     
     protected $guarded = array('id');
     
